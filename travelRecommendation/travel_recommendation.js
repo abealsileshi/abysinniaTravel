6849934e2de069e039f9f4b200057abf0f93813f
abealsileshi <abealsileshi@gmail.com>
2024-10-25 14:46:57 -0600

    Image files added

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch' 
 
 function fetchJSONData() {
-fetch("/Users/abealsileshi/Desktop/Coursera/abysinniaTravel/travelRecommendation/travel_recommendation_api.json")
+fetch('./travelRecommendation/travel_recommendation_api.json')
   .then(response => response.json())
   .then(json => console.log(json));
 }
